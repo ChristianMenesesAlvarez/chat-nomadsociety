7ed37315b9b8eab3d504115986582c219af8dd15
Christian Meneses <christian.meneses.ls@gmail.com>
2023-03-20 16:17:49 +0100

    update

--- src/database.js
+++ src/database.js
@@ -1,9 +1,11 @@
 import Mongoose from 'mongoose';
 
 export async function DBconnection() {
+  const URL = process.env.MONGO_URI2;
+  console.log(URL);
   Mongoose.set('strictQuery', false);
   try {
-    const connection = Mongoose.connect("mongodb+srv://admin:admin@testdb.is3yx9o.mongodb.net/?retryWrites=true&w=majority");
+    const connection = Mongoose.connect(URL);
     const timelog = new Date();
     console.log(`SERVERLOG ${timelog} --> Connected to MongoDB.`);
     return connection;
