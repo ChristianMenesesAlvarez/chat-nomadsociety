659a31a614b8fe86bde02ad482ad7f4d5edcabe1
Christian Meneses <christian.meneses.ls@gmail.com>
2023-03-20 15:56:31 +0100

    update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import { DBconnection } from '../database/databaseConnection.js';
+import { DBconnection } from './database.js';
 import * as usersRepository from '../src/api/usersRepository.js';
 import * as chatRepository from '../src/api/chatRepository.js';
 
